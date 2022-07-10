762dea1705fca57d2267e7fb75069f75dc3050ae
Yair Turco <turcoyair@gmail.com>
2022-07-10 11:54:18 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//FORMULARIO DE REGISTRO - CON PROCESO DE PAGO
+
+function Usuario (userName, email, password, cardUser, cardNumber, cardDate, cardCode, country, state) {
+    this.userName = userName;
+    this.email = email;
+    this.password = password;
+    this.cardUser = cardUser;
+    this.cardNumber = cardNumber;
+    this.cardDate = cardDate;
+    this.cardCode = cardCode;
+    this.country = country;
+    this.state = state;
+}
+
+
+
+//FORMULARIO DE INGRESO
