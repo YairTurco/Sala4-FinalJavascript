59eb69598a7673d5ddafb5224ca89f7b3656050f
Yair Turco <turcoyair@gmail.com>
2022-07-16 09:56:41 -0300

    fix objeto al top

--- js/main.js
+++ js/main.js
@@ -1,18 +1,36 @@
+//ARRAY USUARIOS
+
+let Usuarios = [];
+
+class Usuario {
+    constructor(userName, email, password, cardUser, cardNumber, cardDate, cardCode, country, state, plan) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.cardUser = cardUser;
+        this.cardNumber = cardNumber;
+        this.cardDate = cardDate;
+        this.cardCode = cardCode;
+        this.country = country;
+        this.state = state;
+        this.plan = plan;
+    }
+}
+
+
 //SELECCION PLAN
 
 let planMensual = 1000;
 let planTrimestral = 833.33;
 let planAnual = 666.66;
 
-SeleccionPlan()
-
 function SeleccionPlan (){
     alert("Seleccione el plan de su preferencia para abonar el servicio.");
-    let seleccionPlan = parseInt(prompt("1. Plan mensual: $1000 x mes. \n2. Plan trimestral: $2500 x trimestre. \n3. Plan anual: $8000 x año.\n"+
+    let plan = parseInt(prompt("1. Plan mensual: $1000 x mes. \n2. Plan trimestral: $2500 x trimestre. \n3. Plan anual: $8000 x año.\n"+
     "Ingrese el numero de plan que desea."));
-        switch (seleccionPlan) {
+        switch (plan) {
             case 1:
-                alert (`Seleccionó el plan mensual. El monto final de su pago será de $${planMensual} por mes. Ingrese sus datos a continuación. `);
+                alert (`Seleccionó el plan mensual. El monto final de su pago será de $${planMensual} por mes. Ingrese sus datos a continuación.`);
                 break;
             case 2:
                 alert (`Seleccionó el plan trimestral. El monto final de su pago será de $${planTrimestral} por mes. Ingrese sus datos a continuación.`);
@@ -25,26 +43,15 @@ function SeleccionPlan (){
                 break;
         }
 
-    return seleccionPlan;
+    let cargaUsuario = new Usuario(plan);
+    Usuarios.push(cargaUsuario);
+    console.log(Usuarios);
+    return cargaUsuario;
 }
+
+SeleccionPlan()
 
 //FORMULARIO DE REGISTRO
-
-class Usuario {
-    constructor(userName, email, password, cardUser, cardNumber, cardDate, cardCode, country, state) {
-        this.userName = userName;
-        this.email = email;
-        this.password = password;
-        this.cardUser = cardUser;
-        this.cardNumber = cardNumber;
-        this.cardDate = cardDate;
-        this.cardCode = cardCode;
-        this.country = country;
-        this.state = state;
-    }
-}
-
-let Usuarios = [];
 
 const nuevoUsuario = () => {
     let userName = prompt("Cual es tu nombre?");
@@ -83,3 +90,6 @@ const loginUsuario = () => {
 }
 
 loginUsuario()
+
+//BIBLIOTECA
+
