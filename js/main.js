59d2484e35ce53c8090db75dd6da7793e301f75f
Yair Turco <turcoyair@gmail.com>
2022-07-19 20:50:27 -0300

    filtros y funciones para preentrega

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
-//ARRAY USUARIOS
-
-let Usuarios = [];
-
+//ARRAY Y OBJETO USUARIOS
 class Usuario {
     constructor(userName, email, password, cardUser, cardNumber, cardDate, cardCode, country, state, plan) {
         this.userName = userName;
@@ -17,43 +14,19 @@ class Usuario {
     }
 }
 
+let usuarios = [
+    {userName: "Yair", email: "turcoyair@gmail.com", password: "yair", cardUser: "user", cardNumber: 1234123412341234, cardDate:"02/02", cardCode: 123, country: "Argentina", state: "Cordoba", plan: 2},
+];
 
-//SELECCION PLAN
+
+
+//FORMULARIO DE REGISTRO
 
 let planMensual = 1000;
 let planTrimestral = 833.33;
 let planAnual = 666.66;
 
-function SeleccionPlan (){
-    alert("Seleccione el plan de su preferencia para abonar el servicio.");
-    let plan = parseInt(prompt("1. Plan mensual: $1000 x mes. \n2. Plan trimestral: $2500 x trimestre. \n3. Plan anual: $8000 x año.\n"+
-    "Ingrese el numero de plan que desea."));
-        switch (plan) {
-            case 1:
-                alert (`Seleccionó el plan mensual. El monto final de su pago será de $${planMensual} por mes. Ingrese sus datos a continuación.`);
-                break;
-            case 2:
-                alert (`Seleccionó el plan trimestral. El monto final de su pago será de $${planTrimestral} por mes. Ingrese sus datos a continuación.`);
-                break;
-            case 3:
-                alert(`Seleccionó el plan anual. El monto final de su pago será de $${planAnual} por mes. Ingrese sus datos a continuación.`);
-                break;
-            default:
-                alert("ingrese una opcion valida.");
-                break;
-        }
-
-    let cargaUsuario = new Usuario(plan);
-    Usuarios.push(cargaUsuario);
-    console.log(Usuarios);
-    return cargaUsuario;
-}
-
-SeleccionPlan()
-
-//FORMULARIO DE REGISTRO
-
-const nuevoUsuario = () => {
+function nuevoUsuario() {
     let userName = prompt("Cual es tu nombre?");
     let email = prompt("ingrese su correo electronico.");
     let password = prompt("Ingrese la contraseña deseada.");
@@ -64,20 +37,38 @@ const nuevoUsuario = () => {
     let country = prompt("Pais.");
     let state = prompt("Provincia.");
 
-    let cargaUsuario = new Usuario(userName, email, password, cardUser, cardNumber, cardDate, cardCode, country, state);
-    Usuarios.push(cargaUsuario);
-    console.log(Usuarios);
+    alert("Seleccione el plan de su preferencia para abonar el servicio.");
+    let plan = parseInt(prompt("1. Plan mensual: $1000 x mes. \n2. Plan trimestral: $2500 x trimestre. \n3. Plan anual: $8000 x año.\n" +
+        "Ingrese el numero de plan que desea."));
+    switch (plan) {
+        case 1:
+            alert(`Seleccionó el plan mensual. El monto final de su pago será de $${planMensual} por mes. Ingrese sus datos a continuación.`);
+            break;
+        case 2:
+            alert(`Seleccionó el plan trimestral. El monto final de su pago será de $${planTrimestral} por mes. Ingrese sus datos a continuación.`);
+            break;
+        case 3:
+            alert(`Seleccionó el plan anual. El monto final de su pago será de $${planAnual} por mes. Ingrese sus datos a continuación.`);
+            break;
+        default:
+            alert("ingrese una opcion valida.");
+            break;
+    }
+
+    let cargaUsuario = new Usuario(userName, email, password, cardUser, cardNumber, cardDate, cardCode, country, state, plan);
+    usuarios.push(cargaUsuario);
+    console.log(usuarios);
 
     return cargaUsuario;
 }
 
-nuevoUsuario()
+// nuevoUsuario()
 
 //FORMULARIO DE LOGIN
 
-alert(`Bienvenido a Sala 4!`);
+// alert(`Bienvenido a Sala 4!`);
 
-const loginUsuario = () => {
+function loginUsuario() {
 
     let loginUser = prompt("Nombre de usuario.");
     let loginPass = prompt("ingrese su contraseña.");
@@ -89,7 +80,57 @@ const loginUsuario = () => {
     }
 }
 
-loginUsuario()
+// loginUsuario()
 
-//BIBLIOTECA
+/*--------------------------------------------------------------------------*/
+
+//BIBLIOTECA DE SERIES Y PELICULAS
+
+class content {
+    constructor (tipo, titulo, genero, estreno, puntuacion) {
+        this.tipo = tipo;
+        this.titulo = titulo;
+        this.genero = genero;
+        this.estreno = estreno;
+        this.puntuacion = puntuacion;
+    }
+}
+
+let library = [
+    {tipo: "pelicula", titulo: "el padrino", genero: "drama", estreno: 1972, puntuacion: 87},
+    {tipo: "pelicula", titulo: "parasitos", genero: "drama", estreno: 2019, puntuacion: 85},
+    {tipo: "pelicula", titulo: "el caballero oscuro", genero: "accion", estreno: 2008, puntuacion: 85},
+    {tipo: "pelicula", titulo: "top gun: maverick", genero: "acción", estreno: 2022, puntuacion: 84},
+    {tipo: "serie", titulo: "arcane", genero: "animación", estreno: 2021, puntuacion: 91},
+    {tipo: "serie", titulo: "breaking bad", genero: "drama", estreno: 2008, puntuacion: 88},
+    {tipo: "serie", titulo: "the umbrella academy", genero: "aventura", estreno: 2019, puntuacion: 87},
+    {tipo: "serie", titulo: "stranger things", genero: "aventura", estreno: 2016, puntuacion: 86},
+
+]
+
+//FILTROS
+//por tipo
+const pelicula = library.filter((el) => el.tipo.includes('pelicula'));
+const serie = library.filter((el) => el.tipo.includes('serie'));
+
+console.log(pelicula);
+
+//por puntuación
+const puntonueve = library.filter ((el) => (el.puntuacion <= 100) && (el.puntuacion >= 90))
+const puntoocho = library.filter ((el) => (el.puntuacion <= 89) && (el.puntuacion >= 80));
+const puntosiete = library.filter ((el) => (el.puntuacion <= 79) && (el.puntuacion >= 70));
+
+console.log(puntonueve);
+
+/*--------------------------------------------------------------------------*/
+
+//FAVORITOS
+
+let favorites = [
+
+]
+
+function addFavorites() {
+    //Formula para agregar peliculas o series a favoritos.
+}
 
